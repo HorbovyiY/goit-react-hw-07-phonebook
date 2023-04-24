bdf95a3b74b3340302ea3dc9791145d33b6d3436
HorbovyiY <yhorbovyi@gmail.com>
2023-04-24 09:41:52 +0300

    видаляє роботу з local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contactsReducer from './contactsSlice'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-
-const persistConfig = {
-    key: 'root',
-    storage,
-}
-
-const persistedcontactsReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
     reducer: {
-        contacts: persistedcontactsReducer,
+        contacts: contactsReducer,
     },
 })
-
-export const persistor = persistStore(store)
